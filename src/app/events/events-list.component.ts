81e075dc9593c4a897d924a67dc89c7f202c1147
George Koshy <geo.koshy89@gmail.com>
2020-11-28 22:50:07 +0530

    Added data fetching code life cycle hk function nginit

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { EventService } from './shared/event.service';
 @Component({
     selector:'events-list',
@@ -14,9 +14,13 @@ import { EventService } from './shared/event.service';
     </div>
     `
 })
-export class EventsListComponent{
+export class EventsListComponent implements OnInit{
   events:any[];
     constructor(private eventService:EventService){
-      this.events=this.eventService.getEvents();
+      
     }  
+
+    ngOnInit(){
+      this.events=this.eventService.getEvents();
+    }
 }
