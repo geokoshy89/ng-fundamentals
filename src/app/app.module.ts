0056032f85752ea8c035a07648e91a55a97b184a
George Koshy <geo.koshy89@gmail.com>
2020-12-06 13:08:17 +0530

    Adde changes fo barreling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
-import { EventsAppComponent } from './events-app.component';
-import {EventsListComponent} from './events/events-list.component';
-import {EventThumbnailComponent} from './events/event-thumbnail.component';
+
 import {NavBarComponent} from './nav/navbar.component';
-import {EventService } from './events/shared/event.service';
+import { EventsAppComponent } from './events-app.component';
+
+import { EventsListComponent,
+         EventThumbnailComponent,
+         CreateEventComponent,
+         EventListResolverService ,
+         EventService ,
+         EventRouteActivatot as EventRouteActivator,
+         EventDetailsComponent } from './events/index';
+
 import {ToastrService} from './common/toastr.service';
-import { EventDetailsComponent } from './events/event-details/event-details.component';
 import {appRoutes} from './routes';
-import { CreateEventComponent } from './events/create-event.component';
+
 import { Error404Component } from './errors/404.component';
-import { EventRouteActivatot as EventRouteActivator } from './events/event-details/event-route-activator.service';
-import { EventListResolverService } from './events/event-list-resolver.service';
+
 
 @NgModule({
   imports: [
